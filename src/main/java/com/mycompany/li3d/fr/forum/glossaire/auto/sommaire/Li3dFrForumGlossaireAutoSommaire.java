b42979fdfd416b0581ba28ccc2f32b9dbb4ae789
q6 <q6@q6-pc>
2022-12-30 21:48:26 +0100

    pom.xml

---
+++ src/main/java/com/mycompany/li3d/fr/forum/glossaire/auto/sommaire/Li3dFrForumGlossaireAutoSommaire.java
@@ -0,0 +1,15 @@
+/*
+ */
+
+package com.mycompany.li3d.fr.forum.glossaire.auto.sommaire;
+
+/**
+ *
+ * @author q6
+ */
+public class Li3dFrForumGlossaireAutoSommaire {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
